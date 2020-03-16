e03a51879f0fa7f6a1b9c62871f7000db8b2fae3
Louis Hobson <louis-hobson@hotmail.co.uk>
2020-03-16 20:18:45 +0000

    Defined glh_exception class

---
+++ include/glhelper/glhelper_exception.hpp
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2020 Louis Hobson <louis-hobson@hotmail.co.uk>. All Rights Reserved.
+ * 
+ * Distributed under MIT licence as a part of the GLHelper C++ library.
+ * For details, see: https://github.com/louishobson/GLHelper/blob/master/LICENSE
+ * 
+ * src/glhelper_exception.hpp
+ * 
+ * defines the basis for exception handling for the library
+ * 
+ */
+
+
+
+/* HEADER GUARD */
+#ifndef GLHELPER_EXCEPTION_HPP_INCLUDED
+#define GLHELPER_EXCEPTION_HPP_INCLUDED
+
+
+
+/* INCLUDES */
+
+/* include core headers */
+#include <iostream>
+
+/* include exception */
+#include <exception>
+
+/* include glhelper_core.hpp */
+#include <glhelper/glhelper_core.hpp>
+
+
+
+/* NAMESPACE FORWARD DECLARATIONS */
+
+namespace glh
+{
+    /* class glh_exception : std::exception
+     *
+     * base class for exceptions in glh
+     */
+    class glh_exception;
+}
+
+
+
+/* FULL DECLARATIONS */
+
+/* class glh_exception : std::exception
+ *
+ * base class for exceptions in glh
+ */
+class glh::glh_exception : public std::exception
+{
+    /* _what
+     *
+     * string containing description of the exception
+     */
+    const char * _what; 
+
+
+    
+    /* full constructor
+     *
+     * __what: description of the exception
+     */
+    glh_exception ( const char * __what )
+        : _what { __what }
+    {}
+
+    /* zero-parameter constructor
+     *
+     * sets _what to NULL
+     */
+    glh_exception ()
+        : _what { NULL }
+    {}
+
+    /* default copy constructor */
+    /* default asignment operator */
+    /* default destructor */
+
+    
+
+    /* virtual what
+     *
+     * returns the description of the exception
+     * 
+     * return: _what
+     */
+    virtual const char * what () const throw () { return _what; }
+
+};
+
+
+
+/* #ifndef GLHELPER_EXCEPTION_HPP_INCLUDED */
+#endif
