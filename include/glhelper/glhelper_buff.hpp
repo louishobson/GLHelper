92cee31e94dec63a60ccd797d2e5abd70fcf2e8d
Louis Hobson <louis-hobson@hotmail.co.uk>
2020-03-16 20:36:52 +0000

    Created file

---
+++ include/glhelper/glhelper_buff.hpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2020 Louis Hobson <louis-hobson@hotmail.co.uk>. All Rights Reserved.
+ * 
+ * Distributed under MIT licence as a part of the GLHelper C++ library.
+ * For details, see: https://github.com/louishobson/GLHelper/blob/master/LICENSE
+ * 
+ * src/glhelper_buff.hpp
+ * 
+ * declares buffer handling constructs
+ * 
+ */
+
+
+
+/* HEADER GUARD */
+#ifndef GLHELPER_BUFF_HPP_INCLUDED
+#define GLHELPER_BUFF_HPP_INCLUDED
+
+
+
+/* INCLUDES */
+
+/* include core headers */
+#include <iostream>
+
+/* include glhelper_core.hpp */
+#include <glhelper/glhelper_core.hpp>
+
+/* include glhelper_exception.hpp */
+#include <glhelper/glhelper_exception.hpp>
+
+
+
+/* #ifndef GLHELPER_BUFF_HPP_INCLUDED */
+#endif
