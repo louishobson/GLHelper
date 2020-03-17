a5f2d48cd226e2cf53acf30ba10780600aeef611
Louis Hobson <louis-hobson@hotmail.co.uk>
2020-03-17 19:42:13 +0000

    Created buffer class

--- include/glhelper/glhelper_buff.hpp
+++ include/glhelper/glhelper_buff.hpp
@@ -4,7 +4,7 @@
  * Distributed under MIT licence as a part of the GLHelper C++ library.
  * For details, see: https://github.com/louishobson/GLHelper/blob/master/LICENSE
  * 
- * src/glhelper_buff.hpp
+ * include/glhelper/glhelper_buff.hpp
  * 
  * declares buffer handling constructs
  * 
@@ -28,6 +28,146 @@
 
 /* include glhelper_exception.hpp */
 #include <glhelper/glhelper_exception.hpp>
+
+/* include glhelper_glad.hpp */
+#include <glhelper/glhelper_glad.hpp>
+
+
+
+/* NAMESPACE FORWARD DECLARATIONS */
+
+namespace glh
+{
+    /* class buffer : object
+     *
+     * base class for storing a buffer
+     */
+    class buffer;
+
+    /* class buffer_exception : exception
+     *
+     * for exceptions related to buffers
+     */
+    class buffer_exception;
+}
+
+
+
+/* FULL DECLARATIONS */
+
+/* class buffer : object
+ *
+ * base class for storing a buffer
+ */
+class glh::buffer : public object
+{
+public:
+
+    /* constructor
+     *
+     * generates a buffer
+     */
+    explicit buffer ();
+
+    /* deleted copy constructor
+     *
+     * it makes no sense to copy a buffer
+     */
+    buffer ( const buffer& other ) = delete;
+
+    /* default move constructor */
+    buffer ( buffer&& other ) = default;
+
+    /* deleted copy assignment operator
+     *
+     * it makes no sense to assign the buffer after comstruction
+     */
+    buffer& operator= ( const buffer& other ) = delete;
+
+    /* virtual destructor
+     *
+     * virtual in preparation for polymorphism
+     */
+    virtual ~buffer ();
+
+
+
+    /* pure virtual type
+     *
+     * get the type of the buffer
+     */
+    virtual GLenum type () const = 0;
+
+    /* buffer_data
+     *
+     * buffer data into the buffer
+     */
+    void buffer_data ( const size_t size, const void * data, const GLenum usage );
+
+    /* clear_data
+     *
+     * clear the data from the buffer
+     */
+    void clear_data () { buffer_data ( 0, NULL, GL_STATIC_DRAW ); }
+
+
+
+    /* virtual destroy
+     *
+     * destroys the object, setting id to -1
+     */
+    virtual void destroy () override;
+
+
+
+private:
+
+    /* bind
+     *
+     * bind the buffer
+     * 
+     * return: the target it is bound to
+     */
+    GLenum bind () const { glBindBuffer ( type (), id ); }
+
+    /* unbind
+     *
+     * unbind the buffer's target
+     * 
+     * return: the target just unbound
+     */
+    GLenum unbind () const { glBindBuffer ( type (), id ); }
+
+};
+
+/* class buffer_exception : exception
+ *
+ * for exceptions related to buffers
+ */
+class glh::buffer_exception : public exception
+{
+public:
+
+    /* full constructor
+     *
+     * __what: description of the exception
+     */
+    explicit buffer_exception ( const char * __what )
+        : exception ( __what )
+    {}
+
+    /* zero-parameter constructor
+     *
+     * construct glad_exception with no descrption
+     */
+    explicit buffer_exception ()
+        : exception { NULL }
+    {}
+
+    /* default everything else and inherits what () function */
+
+};
+
 
 
 
---
+++ src/glhelper/glhelper_buff.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2020 Louis Hobson <louis-hobson@hotmail.co.uk>. All Rights Reserved.
+ * 
+ * Distributed under MIT licence as a part of the GLHelper C++ library.
+ * For details, see: https://github.com/louishobson/GLHelper/blob/master/LICENSE
+ * 
+ * src/glhelper/glhelper_buff.cpp
+ * 
+ * implementation of include/glhelper/glhelper_buff.hpp
+ * 
+ */
+
+
+
+/* INCLUDES */
+
+/* include glhelper_buff.hpp */
+#include <glhelper/glhelper_buff.hpp>
+
+
+
+/* BUFFER IMPLEMENTATION */
+
+/* constructor
+ *
+ * generates a buffer
+ */
+glh::buffer::buffer ()
+    : object { -1 }
+{
+    /* allocate the buffer */
+    glGenBuffers ( 1, ( unsigned * ) &id );
+}
+
+/* virtual destructor
+ *
+ * virtual in preparation for polymorphism
+ */
+glh::buffer::~buffer ()
+{
+    /* destroy buffer */
+    if ( id >= 0 ) glDeleteBuffers ( 1, ( unsigned * ) &id );
+}
+
+/* buffer_data
+ *
+ * buffer data into the buffer
+ */
+void glh::buffer::buffer_data ( const size_t size, const void * data, const GLenum usage ) 
+{ 
+    /* bind the buffer */
+    bind ();
+    /* buffer data */
+    glBufferData ( type (), size, data, usage );
+    /* unbind the buffer */
+    unbind ();
+}
+
+/* virtual destroy
+ *
+ * destroys the object, setting id to -1
+ */
+void glh::buffer::destroy ()
+{
+    /* destroy buffer */
+    if ( id >= 0 ) glDeleteBuffers ( 1, ( unsigned * ) id );
+    
+    /* set id to -1 */
+    id = -1;
+}
