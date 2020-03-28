3835f685f492a717c63e653a5ec5a16667625c20
Louis Hobson <louis-hobson@hotmail.co.uk>
2020-03-28 17:51:04 +0000

    Can be used to include only math-related headers

---
+++ include/glhelper/glhelper_math.hpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2020 Louis Hobson <louis-hobson@hotmail.co.uk>. All Rights Reserved.
+ * 
+ * Distributed under MIT licence as a part of the GLHelper C++ library.
+ * For details, see: https://github.com/louishobson/GLHelper/blob/master/LICENSE
+ * 
+ * include/glhelper/glhelper_math.hpp
+ * 
+ * main header for including all math-based header files
+ * 
+ */
+
+
+
+/* INCLUDES */
+
+/* include glhelper_core.hpp */
+#include <glhelper/glhelper_core.hpp>
+
+/* include glhelper_exception.hpp */
+#include <glhelper/glhelper_exception.hpp>
+
+/* include glhelper_matrix.hpp */
+#include <glhelper/glhelper_matrix.hpp>
+
+/* include glhelper_vector.hpp */
+#include <glhelper/glhelper_vector.hpp>
+
+/* include glhelper_transform.hpp */
+#include <glhelper/glhelper_transform.hpp>
+
