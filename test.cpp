24d42b92273dfc5a2cf6d559af7d8495c8acf678
Louis Hobson <louis-hobson@hotmail.co.uk>
2020-04-07 00:45:58 +0100

    Added more models

--- test.cpp
+++ test.cpp
@@ -29,8 +29,12 @@ int main ()
     glh::window window;
     window.set_input_mode ( GLFW_CURSOR, GLFW_CURSOR_DISABLED );
 
-    glh::model::model nanosuit { "./assets/nanosuit", "nanosuit.obj" };
-    glh::model::model reinhardt { "./assets/reinhardt/", "scene.gltf", aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_PreTransformVertices };
+    //glh::model::model nanosuit { "./assets/nanosuit", "nanosuit.obj" };
+    //glh::model::model reinhardt { "./assets/reinhardt", "scene.gltf" };
+    //glh::model::model xenomorphe { "./assets/xenomorphe", "scene.gltf" };
+    //glh::model::model plane { "./assets/plane", "scene.gltf" };
+    //glh::model::model room { "./assets/room", "scene.gltf" };
+    glh::model::model factory { "./assets/factory", "scene.gltf", 0 };
 
     glh::vshader vshader { "shaders/vertex.glsl" };
     glh::fshader fshader { "shaders/fragment.glsl" };
@@ -40,7 +44,7 @@ int main ()
     auto model_uni = trans_uni.get_uniform ( "model" );
     auto normal_uni = trans_uni.get_uniform ( "norm" );
 
-    glh::camera_perspective camera { glh::math::rad ( 75 ), 16.0 / 9.0, 0.1, 1000 };
+    glh::camera_perspective camera { glh::math::rad ( 90 ), 16.0 / 9.0, 0.1, 1000 };
     camera.enable_restrictive_mode ();
     
     glh::renderer::clear_colour ( 1.0, 1.0, 1.0, 1.0 );
@@ -60,13 +64,13 @@ int main ()
             glh::renderer::viewport ( 0, 0, dimensions.width, dimensions.height );
         }
 
-        if ( window.get_key ( GLFW_KEY_W ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { 0., 0., -10. } );
-        if ( window.get_key ( GLFW_KEY_A ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { -10, 0., 0. } );
-        if ( window.get_key ( GLFW_KEY_S ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { 0., 0., 10. } );
-        if ( window.get_key ( GLFW_KEY_D ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { 10, 0., 0. } );
+        if ( window.get_key ( GLFW_KEY_W ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { 0., 0., -100. } );
+        if ( window.get_key ( GLFW_KEY_A ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { -100, 0., 0. } );
+        if ( window.get_key ( GLFW_KEY_S ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { 0., 0., 100. } );
+        if ( window.get_key ( GLFW_KEY_D ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { 100, 0., 0. } );
 
-        if ( window.get_key ( GLFW_KEY_SPACE ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { 0., 10., 0. } );
-        if ( window.get_key ( GLFW_KEY_LEFT_SHIFT ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { 0., -10., 0. } );
+        if ( window.get_key ( GLFW_KEY_SPACE ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { 0., 100., 0. } );
+        if ( window.get_key ( GLFW_KEY_LEFT_SHIFT ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { 0., -100., 0. } );
 
         if ( window.get_key ( GLFW_KEY_Z ).action == GLFW_PRESS ) camera.roll ( timeinfo.delta * glh::math::rad ( 80 ) );
         if ( window.get_key ( GLFW_KEY_X ).action == GLFW_PRESS ) camera.roll ( timeinfo.delta * glh::math::rad ( -80 ) ); 
@@ -78,7 +82,12 @@ int main ()
 
         glh::renderer::clear ();
 
-        nanosuit.render ( material_uni, model_uni, normal_uni, glh::math::resize<4> ( glh::math::enlarge ( glh::math::identity<3> (), 0.01 ) ) );
+        //plane.render ( material_uni, model_uni, normal_uni, glh::math::resize<4> ( glh::math::enlarge ( glh::math::identity<3> (), 0.01 ) ) );
+        //xenomorphe.render ( material_uni, model_uni, normal_uni, glh::math::translate ( glh::math::resize<4> ( glh::math::enlarge ( glh::math::identity<3> (), 0.08 ) ), glh::math::vec3 { 20.0, 0.0, 20.0 } ) );
+        //nanosuit.render ( material_uni, model_uni, normal_uni, glh::math::translate ( glh::math::resize<4> ( glh::math::enlarge ( glh::math::identity<3> (), 0.3 ) ), glh::math::vec3 { -1.0, 0.0, 3.0 } ) );
+        //reinhardt.render ( material_uni, model_uni, normal_uni, glh::math::resize<4> ( glh::math::enlarge ( glh::math::identity<3> (), 0.01 ) ) );
+        //room.render ( material_uni, model_uni, normal_uni, glh::math::resize<4> ( glh::math::enlarge ( glh::math::identity<3> (), 2.0 ) ) );
+        factory.render ( material_uni, model_uni, normal_uni, glh::math::resize<4> ( glh::math::rotate ( glh::math::enlarge ( glh::math::identity<3> (), 1 ), glh::math::rad ( 90 ), 1, 2 ) ) );
 
         window.swap_buffers ();
         window.poll_events ();
