324e0cce47753264ce5532e6574c958470ebd852
Louis Hobson <louis-hobson@hotmail.co.uk>
2020-04-06 02:01:44 +0100

    Minor changes

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -6,242 +6,85 @@
 
 #version 330 core
 
+#define MAX_STACK_SIZE 8
+
+/* structure for a texture */
+struct texture_stack_level_struct
+{
+    int blend_operation;
+    float blend_strength;
+    sampler2D texunit;
+};
+
+/* structure for a texture stack */
+struct texture_stack_struct
+{
+    int stack_size;
+    vec3 base_colour;
+    texture_stack_level_struct levels [ MAX_STACK_SIZE ];
+};
+
 /* structure for a material */
-struct MaterialStruct
+struct material_struct
 {
-    sampler2D Diffuse;
-    sampler2D Specular;
-    float Shininess;
+    texture_stack_struct ambient_stack;
+    texture_stack_struct diffuse_stack;
+    texture_stack_struct specular_stack;
+
+    int blending_mode;
+
+    float shininess;
+    float shininess_strength;
+
+    float opacity;
 };
 
-/* structure for light properties */
-struct LightPropertiesStruct
-{
-    vec3 Ambient;
-    vec3 Diffuse;
-    vec3 Specular;
-};
-
-/* structure for attenuation values */
-struct LightAttenuationStruct
-{
-    float Constant;
-    float Linear;
-    float Quadratic;
-};
-
-/* structure for point light */
-struct PointLightStruct
-{
-    vec3 Position;
-    LightAttenuationStruct Attenuation;
-    LightPropertiesStruct Properties;
-};
-
-/* structure for directional light */
-struct DirLightStruct
-{
-    vec3 Direction;
-    LightPropertiesStruct Properties;
-};
-
-/* structure for spotlights */
-struct SpotLightStruct
-{
-    vec3 Position;
-    vec3 Direction;
-    LightAttenuationStruct Attenuation;
-    LightPropertiesStruct Properties;
-    float Inner;
-    float Outer;
-};
-
-/* texture coords, normal vector and position */
-in vec2 TexCoords;
-in vec3 NormVec;
-in vec3 FragPos;
+/* texture coord, normal vector and position */
+in vec2 texcoord;
+in vec3 norm;
+in vec3 fragpos;
 
 /* output colour */
-out vec4 FragColor;
+out vec4 fragcolour;
 
 /* viewpos, material and lighting uniforms */
-uniform vec3 ViewPos;
-uniform MaterialStruct Material;
-uniform PointLightStruct PointLights [ 2 ];
-uniform DirLightStruct DirLight;
+uniform vec3 viewpos;
+uniform material_struct material;
 
 
 
-/* function to calculate point light colour
+/* evaluate_stack
  *
- * Light: a PointLightStruct for the light
- * 
- * return: the colour of the fragment
- */
-vec3 CalcPointLight ( PointLightStruct Light )
-{
-    /* CALCULATE AMBIENT COMPONENT */
-
-    vec3 Ambient = vec3 ( texture ( Material.Diffuse, TexCoords ) ) * Light.Properties.Ambient;
-
-    /* CALCULATE DIFFUSE COMPONENT */
-
-    /* calculate light direction */
-    vec3 LightDir = normalize ( Light.Position - FragPos );
-    /* calculate diffuse component */
-    float Diff = max ( dot ( NormVec, LightDir ), 0.0 );
-    vec3 Diffuse = vec3 ( texture ( Material.Diffuse, TexCoords ) ) * Diff * Light.Properties.Diffuse;
-
-    /* CALCULATE SPECULAR COMPONENT */
-
-    /* calculate the direction of viewing */
-    vec3 ViewDir = normalize ( ViewPos - FragPos );
-    /* calculate the direction of the reflected light */
-    vec3 ReflectDir = reflect ( - LightDir, NormVec );
-    /* calculate the specular component */
-    float Spec = pow ( max ( dot ( ViewDir, ReflectDir ), 0.0 ), Material.Shininess );
-    vec3 Specular = vec3 ( texture ( Material.Specular, TexCoords ) ) * Spec * Light.Properties.Specular;
-
-    /* ATTENUATE */
-
-    /* calculate attenuation */
-    float Distance = length ( Light.Position - FragPos );
-    float Attenuation = 1.0 / ( Light.Attenuation.Constant + ( Light.Attenuation.Linear * Distance ) + ( Light.Attenuation.Quadratic * Distance * Distance ) );
-
-    /* apply attenuation */
-    Ambient *= Attenuation;
-    Diffuse *= Attenuation;
-    Specular *= Attenuation;
-
-    /* SET THE FRAGMENT COLOUR */
-
-    /* set to a combination of the ambient, diffuce and specular components */
-    return Ambient + Diffuse + Specular;
-}
-
-
-/* function to calculate directional light colour
+ * evaluate multiple stacked textures
  *
- * Light: a DirLightStruct for the light
- * 
- * return: the colour of the fragment
- */
-vec3 CalcDirLight ( DirLightStruct Light )
-{
-    /* CALCULATE AMBIENT COMPONENT */
-
-    vec3 Ambient = vec3 ( texture ( Material.Diffuse, TexCoords ) ) * Light.Properties.Ambient;
-
-    /* CALCULATE DIFFUSE COMPONENT */
-
-    /* calculate light direction */
-    vec3 LightDir = normalize ( - Light.Direction );
-    /* calculate diffuse component */
-    float Diff = max ( dot ( NormVec, LightDir ), 0.0 );
-    vec3 Diffuse = vec3 ( texture ( Material.Diffuse, TexCoords ) ) * Diff * Light.Properties.Diffuse;
-
-    /* CALCULATE SPECULAR COMPONENT */
-
-    /* calculate the direction of viewing */
-    vec3 ViewDir = normalize ( ViewPos - FragPos );
-    /* calculate the direction of the reflected light */
-    vec3 ReflectDir = reflect ( - LightDir, NormVec );
-    /* calculate the specular component */
-    float Spec = pow ( max ( dot ( ViewDir, ReflectDir ), 0.0 ), Material.Shininess );
-    vec3 Specular = vec3 ( texture ( Material.Specular, TexCoords ) ) * Spec * Light.Properties.Specular;
-
-    /* SET THE FRAGMENT COLOUR */
-
-    /* set to a combination of the ambient, diffuce and specular components */
-    return Ambient + Diffuse + Specular;
-}
-
-
-
-/* function to calculate spotlight light colour
+ * material: the material the stack if part of
+ * stack: the stack to evaluate
  *
- * Light: a SpotLightStruct for the light
- * 
- * return: the colour of the fragment
+ * return: the overall colour of the fragment of the stack
  */
-vec3 CalcSpotLight ( SpotLightStruct Light )
+vec4 evaluate_stack ( material_struct mat, texture_stack_struct stack )
 {
-    /* CALCULATE AMBIENT COMPONENT */
+    /* get base colour */
+    vec4 stack_colour = vec4 ( stack.base_colour, 1.0 );
 
-    vec3 Ambient = vec3 ( texture ( Material.Diffuse, TexCoords ) ) * Light.Properties.Ambient;
-
-    /* CALCULATE ATTENUATION */
-
-    /* calculate attenuation */
-    float Distance = length ( Light.Position - FragPos );
-    float Attenuation = 1.0 / ( Light.Attenuation.Constant + ( Light.Attenuation.Linear * Distance ) + ( Light.Attenuation.Quadratic * Distance * Distance ) );
-
-    /* CHECK WHETHER WITHIN SPOTLIGHT
-
-    /* calculate light direction */
-    vec3 LightDir = normalize ( Light.Position - FragPos );
-    /* calculate angle */
-    float Angle = acos ( dot ( LightDir, normalize ( - Light.Direction ) ) );
-
-    /* if within outer */
-    if ( Angle < Light.Outer )
+    /* loop through the stack */
+    for ( int i = 0; i < stack.stack_size; ++i )
     {
-        /* CALCULATE DIFFUSE COMPONENT */
-
-        /* calculate diffuse component */
-        float Diff = max ( dot ( NormVec, LightDir ), 0.0 );
-        vec3 Diffuse = vec3 ( texture ( Material.Diffuse, TexCoords ) ) * Diff * Light.Properties.Diffuse;
-
-        /* CALCULATE SPECULAR COMPONENT */
-
-        /* calculate the direction of viewing */
-        vec3 ViewDir = normalize ( ViewPos - FragPos );
-        /* calculate the direction of the reflected light */
-        vec3 ReflectDir = reflect ( - LightDir, NormVec );
-        /* calculate the specular component */
-        float Spec = pow ( max ( dot ( ViewDir, ReflectDir ), 0.0 ), Material.Shininess );
-        vec3 Specular = vec3 ( texture ( Material.Specular, TexCoords ) ) * Spec * Light.Properties.Specular;
-
-        /* if between inner and outer, reduce intensity */
-        if ( Angle > Light.Inner )
-        {
-            /* CALCULATE DROP IN INTENSITY */
-
-            float Intensity = clamp ( ( Light.Outer - Angle ) / ( Light.Outer - Light.Inner ), 0.0, 1.0 );
-            Diffuse *= Intensity;
-            Specular *= Intensity;
-        }
-
-        /* ATTENUATE */
-
-        /* apply attenuation */
-        Ambient *= Attenuation;
-        Diffuse *= Attenuation;
-        Specular *= Attenuation;
-
-
-        /* RETURN THE COLOUR */
-
-        return Ambient + Diffuse + Specular;
+        /* get the colour */
+        stack_colour += texture ( stack.levels [ i ].texunit, texcoord ) * stack.levels [ i ].blend_strength;
+        /* add to the stack */
     }
 
-    /* OUTSIDE SPOTLIGHT */
-
-    /* apply attenuation */
-    Ambient *= Attenuation;
-
-    /* just return ambient light */
-    return Ambient;
+    /* return the stack colour */
+    return stack_colour;
 }
-
 
 
 
 /* main */
 void main ()
 {
-    FragColor = vec4 ( CalcPointLight ( PointLights [ 0 ] ) + 
-                       CalcPointLight ( PointLights [ 1 ] ) +
-                       CalcDirLight ( DirLight )
-                       , 1.0 );
+    fragcolour = vec4 ( vec3 ( evaluate_stack ( material, material.ambient_stack )
+                 + evaluate_stack ( material, material.diffuse_stack )
+                 + evaluate_stack ( material, material.specular_stack ) ).xyz / 4, 1.0 );
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -7,36 +7,36 @@
 #version 330 core
 
 /* transformations structure */
-struct TransStructure
+struct trans_struct
 {
-    mat4 Model;
-    mat4 View;
-    mat4 Proj;
-    mat3 NormMat;
+    mat4 model;
+    mat4 view;
+    mat4 proj;
+    mat3 norm;
 };
 
 /* vertices, texture coords and normal */
-layout ( location = 0 ) in vec3 aPos;
-layout ( location = 1 ) in vec2 aTexCoords;
-layout ( location = 2 ) in vec3 aNormVec;
+layout ( location = 0 ) in vec3 pos;
+layout ( location = 1 ) in vec3 in_norm;
+layout ( location = 2 ) in vec2 in_texcoord;
 
 /* texture coords, normal */
-out vec2 TexCoords;
-out vec3 NormVec;
-out vec3 FragPos;
+out vec2 texcoord;
+out vec3 norm;
+out vec3 fragpos;
 
 /* transformation matrices */
-uniform TransStructure Trans;
+uniform trans_struct trans;
 
 /* main */
 void main ()
 {
     /* set the position to be the same as the attribute, with an alpha of 1.0 */
-    gl_Position = Trans.Proj * Trans.View * Trans.Model * vec4 ( aPos, 1.0f );
-    /* set TexCoords to aTexCoords */
-    TexCoords = aTexCoords;
-    /* set NormVec to NormMat * aNormVec */
-    NormVec = normalize ( Trans.NormMat * aNormVec );
-    /* set FragPos */
-    FragPos = vec3 ( Trans.Model * vec4 ( aPos, 1.0 ) );
+    gl_Position = trans.proj * trans.view * trans.model * vec4 ( pos, 1.0f );
+    /* set texcoord to in_texcoords */
+    texcoord = in_texcoord;
+    /* set norm to trans.norm * in_norm */
+    norm = normalize ( trans.norm * in_norm );
+    /* set fragpos */
+    fragpos = vec3 ( trans.model * vec4 ( pos, 1.0 ) );
 }
