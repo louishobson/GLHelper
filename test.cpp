b40ca62a7ddf332767c058b0b6f693a27da485fd
Louis Hobson <louis-hobson@hotmail.co.uk>
2020-04-08 18:39:50 +0100

    Lighting

--- test.cpp
+++ test.cpp
@@ -47,17 +47,25 @@ int main ()
     glh::camera_perspective camera { glh::math::rad ( 90 ), 16.0 / 9.0, 0.1, 500.0 };
     camera.enable_restrictive_mode ();
     
-    glh::renderer::clear_color ( 1.0, 0.0, 1.0, 1.0 );
+    glh::renderer::clear_color ( 1.0, 1.0, 1.0, 1.0 );
     glh::renderer::enable_depth_test ();
 
     program.use ();
+    glh::math::vec3 light_position { 0.0, 0.0, -60.0 };
     trans_uni.get_uniform ( "proj" ).set_matrix ( camera.get_proj () );
     lighting_uni.get_uniform ( "directional_size" ).set_int ( 1 );
-    lighting_uni.get_uniform ( "point_size" ).set_int ( 0 );
+    lighting_uni.get_uniform ( "point_size" ).set_int ( 1 );
     lighting_uni.get_array_uniform<glh::struct_uniform> ( "directional" ).at ( 0 ).get_uniform ( "direction" ).set_vector ( glh::math::vec3 { 0.0, -1.0, 0.0 } );
-    lighting_uni.get_array_uniform<glh::struct_uniform> ( "directional" ).at ( 0 ).get_uniform ( "ambient_color" ).set_vector ( glh::math::vec3 { 0.1, 0.1, 0.1 } );
-    lighting_uni.get_array_uniform<glh::struct_uniform> ( "directional" ).at ( 0 ).get_uniform ( "diffuse_color" ).set_vector ( glh::math::vec3 { 0.5, 0.5, 0.5 } );
-    lighting_uni.get_array_uniform<glh::struct_uniform> ( "directional" ).at ( 0 ).get_uniform ( "specular_color" ).set_vector ( glh::math::vec3 { 1.0, 1.0, 1.0 } );
+    lighting_uni.get_array_uniform<glh::struct_uniform> ( "directional" ).at ( 0 ).get_uniform ( "ambient_color" ).set_vector ( glh::math::vec3 { 0.4, 0.4, 0.4 } );
+    lighting_uni.get_array_uniform<glh::struct_uniform> ( "directional" ).at ( 0 ).get_uniform ( "diffuse_color" ).set_vector ( glh::math::vec3 { 0.4, 0.4, 0.4 } );
+    lighting_uni.get_array_uniform<glh::struct_uniform> ( "directional" ).at ( 0 ).get_uniform ( "specular_color" ).set_vector ( glh::math::vec3 { 0.7, 0.7, 0.7 } );
+    lighting_uni.get_array_uniform<glh::struct_uniform> ( "point" ).at ( 0 ).get_uniform ( "position" ).set_vector ( light_position );
+    lighting_uni.get_array_uniform<glh::struct_uniform> ( "point" ).at ( 0 ).get_uniform ( "ambient_color" ).set_vector ( glh::math::vec3 { 0.0, 0.0, 0.0 } );
+    lighting_uni.get_array_uniform<glh::struct_uniform> ( "point" ).at ( 0 ).get_uniform ( "diffuse_color" ).set_vector ( glh::math::vec3 { 1.0, 1.0, 1.0 } );
+    lighting_uni.get_array_uniform<glh::struct_uniform> ( "point" ).at ( 0 ).get_uniform ( "specular_color" ).set_vector ( glh::math::vec3 { 1.0, 1.0, 1.0 } );
+    lighting_uni.get_array_uniform<glh::struct_uniform> ( "point" ).at ( 0 ).get_uniform ( "att_const" ).set_float ( 1.0 );
+    lighting_uni.get_array_uniform<glh::struct_uniform> ( "point" ).at ( 0 ).get_uniform ( "att_linear" ).set_float ( 0.011 );
+    lighting_uni.get_array_uniform<glh::struct_uniform> ( "point" ).at ( 0 ).get_uniform ( "att_quad" ).set_float ( 0.0007 );
 
     window.get_mouseinfo ();
     auto dimensions = window.get_dimensions ();
@@ -78,6 +86,7 @@ int main ()
             glh::renderer::viewport ( 0, 0, dimensions.width, dimensions.height );
         }
 
+
         if ( window.get_key ( GLFW_KEY_W ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { 0., 0., -15. } );
         if ( window.get_key ( GLFW_KEY_A ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { -15, 0., 0. } );
         if ( window.get_key ( GLFW_KEY_S ).action == GLFW_PRESS ) camera.move ( timeinfo.delta * glh::math::vec3 { 0., 0., 15. } );
@@ -94,15 +103,17 @@ int main ()
 
         trans_uni.get_uniform ( "view" ).set_matrix ( camera.get_view () );
         trans_uni.get_uniform ( "viewpos" ).set_vector ( camera.get_pos () );
+        light_position = glh::math::rotate ( light_position, glh::math::rad ( 0.5 ), glh::math::vec3 ( 1.0, 0.0, 0.0 ) );
+        lighting_uni.get_array_uniform<glh::struct_uniform> ( "point" ).at ( 0 ).get_uniform ( "position" ).set_vector ( light_position );
 
         glh::renderer::clear ();
 
         //plane.render ( material_uni, model_uni, glh::math::resize<4> ( glh::math::enlarge ( glh::math::identity<3> (), 0.01 ) ) );
         //xenomorphe.render ( material_uni, model_uni, glh::math::translate ( glh::math::resize<4> ( glh::math::enlarge ( glh::math::identity<3> (), 0.08 ) ), glh::math::vec3 { 20.0, 0.0, 20.0 } ) );
-        nanosuit.render ( material_uni, model_uni, glh::math::translate ( glh::math::resize<4> ( glh::math::enlarge ( glh::math::identity<3> (), 1 ) ), glh::math::vec3 { 0.0, 0.0, 0.0 } ) );
+        //nanosuit.render ( material_uni, model_uni, glh::math::translate ( glh::math::resize<4> ( glh::math::enlarge ( glh::math::identity<3> (), 1 ) ), glh::math::vec3 { 0.0, 0.0, 0.0 } ) );
         //reinhardt.render ( material_uni, model_uni, glh::math::resize<4> ( glh::math::enlarge ( glh::math::identity<3> (), 0.01 ) ) );
         //room.render ( material_uni, model_uni, glh::math::resize<4> ( glh::math::enlarge ( glh::math::identity<3> (), 2.0 ) ) );
-        factory.render ( material_uni, model_uni, glh::math::resize<4> ( glh::math::rotate ( glh::math::enlarge ( glh::math::identity<3> (), 0.1 ), glh::math::rad ( 90 ), 1, 2 ) ) );
+        factory.render ( material_uni, model_uni, glh::math::translate ( glh::math::resize<4> ( glh::math::rotate ( glh::math::enlarge ( glh::math::identity<3> (), 0.1 ), glh::math::rad ( 90 ), 1, 2 ) ), glh::math::vec3 { -10.0, 0.0, -20.0 } ) );
 
         window.swap_buffers ();
         window.poll_events ();
