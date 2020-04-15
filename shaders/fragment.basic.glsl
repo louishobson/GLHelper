b86e86f05084178c87f2c7e0fc494042faf6f05f
Louis Hobson <louis-hobson@hotmail.co.uk>
2020-04-15 20:25:42 +0100

    Created basic fragment shader

---
+++ shaders/fragment.basic.glsl
@@ -0,0 +1,28 @@
+/*
+ * fragment.basic.glsl
+ * 
+ * basic fragment shader
+ */
+
+#version 330 core
+
+/* texture coords, normal vector and position */
+in vec2 texcoords [ 8 ];
+in vec3 normal;
+in vec3 fragpos;
+
+/* output color */
+out vec4 fragcolor;
+
+/* sampler uniform */
+uniform sampler2D texunit;
+
+
+
+/* main */
+void main ()
+{
+    /* sample texture and return */
+    fragcolor = texture ( texunit, texcoords [ 0 ] );
+    //fragcolor = vec4 ( 0.0, 0.0, 0.0, 1.0 );
+}
