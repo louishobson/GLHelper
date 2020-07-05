33dcdee4a923810ba5599a1d0b5fcf3804a99365
Louis Hobson <louis-hobson@hotmail.co.uk>
2020-07-05 17:01:53 +0100

    Now calculates and outputs TBN matrix rather than just the normal

--- shaders/vertex.model.glsl
+++ shaders/vertex.model.glsl
@@ -11,14 +11,15 @@
 /* input vertex position, normal, vertex color, and texture coords */
 layout ( location = 0 ) in vec3 in_pos;
 layout ( location = 1 ) in vec3 in_normal;
-layout ( location = 2 ) in vec4 in_vcolor;
-layout ( location = 3 ) in vec3 in_texcoords [ MAX_TEXTURE_STACK_SIZE ];
+layout ( location = 2 ) in vec3 in_tangent;
+layout ( location = 3 ) in vec4 in_vcolor;
+layout ( location = 4 ) in vec3 in_texcoords [ MAX_TEXTURE_STACK_SIZE ];
 
-/* output fragpos, normal, vcolor and texcoords */
+/* output fragpos, tbn_matrix, vcolor and texcoords */
 out VS_OUT
 {
     vec3 fragpos;
-    vec3 normal;
+    mat3 tbn_matrix;
     vec4 vcolor;
     vec3 texcoords [ MAX_TEXTURE_STACK_SIZE ];
 } vs_out;
@@ -40,11 +41,11 @@ void main ()
     /* transform position based on camera */
     gl_Position = camera.view_proj * vec4 ( in_pos, 1.0 );
 
-    /* pass through normal */
-    vs_out.normal = in_normal;
-
     /* set fragpos */
     vs_out.fragpos = in_pos;
+
+    /* set tbn matrix */
+    vs_out.tbn_matrix = mat3 ( in_tangent, cross ( in_normal, in_tangent ), in_normal );
 
     /* set vcolor */
     vs_out.vcolor = in_vcolor;
