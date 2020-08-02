166febc4898b1954a971a4741d607dab4566a210
Louis Hobson <louis-hobson@hotmail.co.uk>
2020-08-02 15:30:25 +0100

    Optimisations

--- shaders/fragment.model.glsl
+++ shaders/fragment.model.glsl
@@ -49,15 +49,11 @@ uniform int transparent_mode;
 /* main */
 void main ()
 {
-    /* evaluate stacks */
-    vec4 ambient; evaluate_stack_macro ( ambient, material.diffuse_stack, vs_out.texcoords );
-    vec4 diffuse = ambient;
+    /* evaluate color stacks */
+    vec4 diffuse; evaluate_stack_macro ( diffuse, material.diffuse_stack, vs_out.texcoords );
     vec4 specular; evaluate_stack_macro ( specular, material.specular_stack, vs_out.texcoords );
 
-    /* evaluate new normal */
-    vec3 normal; evaluate_normal_macro ( normal, material.normal_stack, vs_out.texcoords, vs_out.tbn_matrix );
-
-    /* discard if opaque/transparent */
+    /* discard if opaque/transparent depending on transparent_mode */
     switch ( transparent_mode )
     {
         case 0: break;
@@ -66,8 +62,11 @@ void main ()
         default: break;
     }
 
+    /* evaluate normal */
+    vec3 normal; evaluate_normal_macro ( normal, material.normal_stack, vs_out.texcoords, vs_out.tbn_matrix );
+
     /* main output color */
-    compute_lighting_macro ( main_fragcolor.xyz, ambient.xyz, diffuse.xyz, specular.xyz, material.shininess, material.shininess_strength, vs_out.fragpos, camera.viewpos, normal, light_system );
+    compute_lighting_macro ( main_fragcolor.xyz, diffuse.xyz, diffuse.xyz, specular.xyz, material.shininess, material.shininess_strength, vs_out.fragpos, camera.viewpos, normal, light_system );
     //compute_lighting_macro ( main_fragcolor.xyz, ambient.xyz, diffuse.xyz, 0.5.xxx, 128, 0.5, vs_out.fragpos, camera.viewpos, normal, light_system );
     main_fragcolor.w = diffuse.a * material.opacity;
 
